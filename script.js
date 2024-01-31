a5e69361c4426e9aa30da9da2d138561df4ee1bd
jfbonglet <jean.francois.bonglet@gmail.com>
2024-01-31 14:00:49 +0100

    update fetch /trips  get method with querystring vs body

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const searchBtn = document.querySelector('#searchBtn')
 
 
 const patternDepArr = /^(?!.*\s{2})[a-z]+(?:-[a-z]+|\s+[a-z]+)*$/i;
-const patternDat = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/
+const patternDat = /^\d{4}-\d{2}-\d{2}$/
 const url = ('http://localhost:3000/trips/')
 
 //Positionne un écouteur sur le bouton recherche
 searchBtn.addEventListener('click', ()=> {
     //Recuperation des valeurs saisies dans les champs
+    document.querySelector('#inputArr').style.borderColor = 'black';
+    document.querySelector('#inputDep').style.borderColor = 'black';
+    document.querySelector('#inputDat').style.borderColor = 'black';
+
     const inputDep = document.querySelector('#inputDep').value
     const inputArr = document.querySelector('#inputArr').value
     const inputDat = document.querySelector('#inputDat').value
+    console.log(inputDat)
     //Test sur les valeurs saisie dans les champs
     if (!(patternDepArr.test(inputDep)))
     {
@@ -31,25 +36,28 @@ searchBtn.addEventListener('click', ()=> {
         return alert('Vérifiez le format des champs saisis')
     }
     
-    //Appel de la route GET/trips pour récupérer les voyages correspondant aux critères de recherche
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("departure", inputDep);
-    urlencoded.append("arrival", inputArr);
-    urlencoded.append("date", inputDat);
-
-    var requestOptions = {
+    // Création de la chaîne de requête en ajoutant les paramètres d'URL
+var queryString = new URLSearchParams({
+    departure: inputDep,
+    arrival: inputArr,
+    date: inputDat
+  }).toString();
+  console.log(queryString)
+  
+  // Construction de l'URL complète avec les paramètres de requête
+  var url = "http://localhost:3000/trips/?" + queryString;
+  
+  var requestOptions = {
     method: 'GET',
-    headers: myHeaders,
-    body: urlencoded,
     redirect: 'follow'
-    };
-
-    fetch("http://localhost:3000/trips/", requestOptions)
+  };
+  
+  fetch(url, requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result)) //TODO mettre à jour la div pour ajouter les cartes de trajet
-    .catch(error => console.log('error', error)); //TODO mettre à jour la div pour changer l'image de résultat
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+ //TODO mettre à jour la div pour ajouter les cartes de trajet
+ //TODO mettre à jour la div pour changer l'image de résultat
 
 })
